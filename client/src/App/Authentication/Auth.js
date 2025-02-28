fb904bb8ba976e96e391bf74841571e2deab7f83
danzilla <dan.alphonza@gmail.com>
2025-02-27 23:30:55 -0500

    Added Ant Design

--- client/src/App/Authentication/Auth.js
+++ client/src/App/Authentication/Auth.js
@@ -1,15 +1,16 @@
+import { Col, Row } from 'antd';
 
 export default function Auth() {
 
   const title = 'Authentication Page'
 
   return (
-    <div className="App">
-      <h1>{title}</h1>
-
-      <a href="/Dashboard">Dashboard</a>
-
-    </div>
+    <Row style={{ height: '100vh' }} type="flex" justify="center" align="middle">
+        <Col className="card-3" style={{padding: '20px'}}>
+            <h1 style={{ color: 'black' }}>{title}</h1>
+            <a href="/Dashboard">Dashboard</a>
+        </Col>
+    </Row>
   );
 }
 
--- client/src/App/Dashboard/Dashboard.js
+++ client/src/App/Dashboard/Dashboard.js
@@ -1,13 +1,16 @@
+import { Col, Row } from 'antd';
 
 export default function Dashboard() {
 
   const title = 'Dashboard Page'
 
   return (
-    <div className="App">
-      <h1>{title}</h1>
-      <a href="/">Home</a>
-    </div>
+    <Row style={{ height: '100vh' }} type="flex" justify="center" align="middle">
+        <Col className="card-3" style={{padding: '20px'}}>
+            <h1 style={{ color: 'black' }}>{title}</h1>
+            <a href="/">Home</a>
+        </Col>
+    </Row>
   );
 }
 
